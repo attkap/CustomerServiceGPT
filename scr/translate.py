6e1163c678aa23299bd31d088e7cea47e6a367db
attkap <attkap@gmail.com>
2023-07-11 14:54:59 +0200

    Change all paths to relative

--- scr/translate.py
+++ scr/translate.py
@@ -3,7 +3,7 @@ import openai
 from dotenv import load_dotenv
 
 def translate_text(customer_request):
-    load_dotenv("/Users/attilakaplan/CustomerBotMVP/.env")
+    load_dotenv(".env")
 
     openai.api_key = os.environ["OpenAI_API_KEY"]
 
