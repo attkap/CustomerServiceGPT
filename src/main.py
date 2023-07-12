1813a415ab8880bd34c384c3f73b30c2925c0dff
attkap <attkap@gmail.com>
2023-07-12 15:39:52 +0200

    put everything together, refactor main.py, change to translate, respond_to_request and categorize.py

--- src/main.py
+++ src/main.py
@@ -1,44 +1,50 @@
-# src/main.py
-#from translate import translate_text
-#from data_saver import save_output
-import json
 import os
+import json
+from translate_request import translate_request
 from categorize import get_parent_category_and_child_category
-from load_data import load_json
-from load_data import load_text_file
+from respond_to_request import get_response
+from save_data import save_output
+from load_data import load_json, load_text_file
 
-
-
-def process_file(): #input_file_path
-    # Read the contents of the input file
-    #with open(input_file_path, 'r') as f:
-    #    customer_request = f.read()
-
-    # Translate the customer request
-    #result = translate_text(customer_request)
-
-    # Save the translation result to a JSON file
-    #save_output(result, input_file_path)
-
-    # Example usage
-    #input_file_path = "../data/customer_requests/customer_request_1.txt"
-    #process_file(input_file_path)
-
-    # Load Data
+def process_files():
+    # Paths
     base_path = "../data"
-    try:
-        categories_list = load_json(
-            os.path.join(base_path, "company_setup"), "category_list.json"
-        )
-        customer_request = load_text_file(
-            os.path.join(base_path, "customer_requests"), "customer_request_1.txt"
-        )
-    except (FileNotFoundError, json.JSONDecodeError) as e:
-        logger.error("Error loading data: %s", e)
-        raise
+    requests_dir = os.path.join(base_path, "customer_requests")
+    outputs_dir = os.path.join(base_path, "LLM_outputs")
 
-    cat = get_parent_category_and_child_category(customer_request, categories_list)
+    # Load categories
+    categories_list = load_json(os.path.join(base_path, "company_setup"), "category_list.json")
+
+    # Loop over all files in the customer_requests directory
+    for filename in os.listdir(requests_dir):
+        if filename.endswith(".txt"):
+            # Load customer request
+            customer_request = load_text_file(requests_dir, filename)
+
+            # Translate the customer request
+            translation_result = translate_request(customer_request)
+
+            # Categorize the translated request
+            categories_result = get_parent_category_and_child_category(translation_result["translated_request"], categories_list)
+
+            # Formulate a response
+            category_context_file = f"{categories_result['parent_category']}_{categories_result['child_category']}.txt"
+            category_context_dir = os.path.join(base_path, "category_contexts")
+            category_context = load_text_file(category_context_dir, category_context_file)
+
+            response_result = get_response(translation_result["translated_request"], categories_result["parent_category"], categories_result["child_category"], category_context)
+
+            # Prepare output
+            output = {
+                "customer_request": customer_request,
+                "translation_result": translation_result,
+                "categories_result": categories_result,
+                "response_result": response_result,
+            }
+
+            # Save output
+            save_output(output, filename, outputs_dir)
 
 
 if __name__ == "__main__":
-    process_file()
+    process_files()
--- src/respond_to_request.py
+++ src/respond_to_request.py
@@ -1,47 +1,21 @@
 import logging
-
 from api import call_llm
 from load_data import load_text_file
 
 # Configure the root logger to log DEBUG and above to the console.
 logging.basicConfig(level=logging.DEBUG, format="%(levelname)s:%(message)s")
 
-# Simulating the output of your previous function
-output1 = {"parent_category": "Dummy_Category", "child_category": "Dummy_Subcategory"}
-
-# Access the parent category and child category
-parent_category = output1["parent_category"]
-subcategory = output1["child_category"]
-
-# Log the parent category and child category
-logging.info(f"Parent Category: {parent_category}, Child Category: {child_category}")
-
-# Simulating the output of another function
-output2 = {
-    "detected_language": "Dummy Language",
-    "translated_request": "Dummy Translated Request",
-}
-
-# Access the detected language and translated request
-detected_language = output2["detected_language"]
-translated_request = output2["translated_request"]
-
-# Log the detected language and translated request
-logging.info(f"Detected Language: {detected_language}, "
-             f"Translated Request: {translated_request}")
-
-# Now, we'll load the context file based on the given parent category and child category
-category_context_file = f"{parent_category}_{child_category}.txt"
-base_path = "../data/category_contexts"
-category_context = load_text_file(base_path, category_context_file)
-
-if category_context is not None:
-    logging.info(f"Loaded context: {category_context}")
-else:
-    logging.error(f"Failed to load context file {category_context_file}")
-
-
 def get_response(translated_request, parent_category, child_category, category_context):
+    # Log the parent category, child category, and translated request
+    logging.info(f"Parent Category: {parent_category}")
+    logging.info(f"Child Category: {child_category}")
+    logging.info(f"Translated Request: {translated_request}")
+
+    if category_context is not None:
+        logging.info(f"Loaded context: {category_context}")
+    else:
+        logging.error(f"Failed to load context file {category_context}")
+
     # Prepare the messages to send to OpenAI
     system_message = f"""
         You are a customer service representative for Saturo. Saturo is a European company that specializes in creating nutritionally complete, ready-to-drink meals and powders that aim to provide balanced nutrition in a convenient format. Your name is Gregor.
@@ -65,8 +39,3 @@ def get_response(translated_request, parent_category, child_category, category_c
     logging.info("Received response from OpenAI API: %s", assistant_response)
 
     return assistant_response
-
-
-assistant_response = get_response(
-    translated_request, parent_category, child_category, category_context
-)
