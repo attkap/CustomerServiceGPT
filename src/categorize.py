e78d6b8f68d3084d1ae4570a2b8847b94a77de93
hannesvienna9000 <feistenauer@gmail.com>
2023-07-12 13:15:23 +0200

    Clean categorize.py methods

--- src/categorize.py
+++ src/categorize.py
@@ -12,51 +12,8 @@ logging.basicConfig(level=logging.INFO)
 # Get a logger instance
 logger = logging.getLogger(__name__)
 
-# Load Data
-base_path = "../data"
-try:
-    categories = load_data.load_json(
-        os.path.join(base_path, "company_setup"), "category_list.json"
-    )
-    customer_request = load_data.load_text_file(
-        os.path.join(base_path, "customer_requests"), "customer_request_1.txt"
-    )
-except (FileNotFoundError, json.JSONDecodeError) as e:
-    logger.error("Error loading data: %s", e)
-    raise
-
-
 # Categorization
-def get_subcategory(customer_request: str, category: str) -> str:
-    """
-    Categorize the customer request into one of the predefined subcategories
-    under the given category.
-
-    This function makes a call to OpenAI API to classify the customer request
-    into a subcategory within the specified category.
-
-    Parameters:
-    customer_request (str): The customer request text.
-    category (str): The category under which to classify the request.
-
-    Returns:
-    str: The subcategory into which the request was classified.
-    """
-    system_message = (
-        "Categorize the customer request into one of the following subcategories:\n"
-        + "\n".join(categories[category])
-    )
-    user_message = customer_request
-
-    try:
-        subcategorization = call_llm(system_message, user_message)
-        return subcategorization.choices[0].message.content.strip()
-    except Exception as e:
-        logger.error("Error subcategorizing request: %s", e)
-        raise
-
-
-def get_category(customer_request: str) -> Dict[str, Optional[str]]:
+def get_category(customer_request: str, categories) -> Dict[str, Optional[str]]:
     """
     Categorize the customer request into one of the predefined categories.
 
@@ -77,28 +34,57 @@ def get_category(customer_request: str) -> Dict[str, Optional[str]]:
 
     try:
         categorization = call_llm(system_message, user_message)
-        category = categorization.choices[0].message.content.strip()
+        parent_category = categorization.choices[0].message.content.strip()
     except Exception as e:
         logger.error("Error categorizing request: %s", e)
         raise
 
-    if category == "Other":
-        subcategory = None
-    else:
-        try:
-            subcategory = get_subcategory(customer_request, category)
-        except Exception as e:
-            logger.error("Error getting subcategory: %s", e)
-            raise
+    return parent_category
 
+def get_subcategory(customer_request: str, categories) -> str:
+    """
+    Categorize the customer request into one of the predefined subcategories
+    under the given category.
+
+    This function makes a call to OpenAI API to classify the customer request
+    into a subcategory within the specified category.
+
+    Parameters:
+    customer_request (str): The customer request text.
+    category (str): The category under which to classify the request.
+
+    Returns:
+    str: The subcategory into which the request was classified.
+    """
+    system_message = (
+        "Categorize the customer request into one of the following subcategories:\n"
+        + "\n".join(categories.keys())
+    )
+    user_message = customer_request
+
+    try:
+        subcategorization = call_llm(system_message, user_message)
+        child_category = subcategorization.choices[0].message.content.strip()
+    except Exception as e:
+        logger.error("Error subcategorizing request: %s", e)
+        raise
+
+    return child_category
+
+
+def get_parent_category_and_child_category(customer_request, categories):
+    try:
+        category = get_category(customer_request, categories)
+    except Exception as e:
+        logger.error("Error getting subcategory: %s", e)
+        raise
     logger.info("Category: %s", category)
+
+    try:
+        subcategory = get_subcategory(customer_request, categories)
+    except Exception as e:
+        logger.error("Error getting subcategory: %s", e)
+        raise
     logger.info("Subcategory: %s", subcategory)
 
-    return {"category": category, "subcategory": subcategory}
-
-
-try:
-    results = get_category(customer_request)
-except Exception as e:
-    logger.error("Error executing categorization: %s", e)
-    raise
+    return {"parent_category": category, "child_category": subcategory}
--- src/main.py
+++ src/main.py
@@ -1,17 +1,44 @@
-from translate import translate_text
-from data_saver import save_output
+# src/main.py
+#from translate import translate_text
+#from data_saver import save_output
+import json
+import os
+from categorize import get_parent_category_and_child_category
+from load_data import load_json
+from load_data import load_text_file
 
-def process_file(input_file_path):
+
+
+def process_file(): #input_file_path
     # Read the contents of the input file
-    with open(input_file_path, 'r') as f:
-        customer_request = f.read()
+    #with open(input_file_path, 'r') as f:
+    #    customer_request = f.read()
 
     # Translate the customer request
-    result = translate_text(customer_request)
+    #result = translate_text(customer_request)
 
     # Save the translation result to a JSON file
-    save_output(result, input_file_path)
+    #save_output(result, input_file_path)
 
-# Example usage
-input_file_path = "../data/customer_requests/customer_request_1.txt"
-process_file(input_file_path)
+    # Example usage
+    #input_file_path = "../data/customer_requests/customer_request_1.txt"
+    #process_file(input_file_path)
+
+    # Load Data
+    base_path = "../data"
+    try:
+        categories = load_json(
+            os.path.join(base_path, "company_setup"), "category_list.json"
+        )
+        customer_request = load_text_file(
+            os.path.join(base_path, "customer_requests"), "customer_request_1.txt"
+        )
+    except (FileNotFoundError, json.JSONDecodeError) as e:
+        logger.error("Error loading data: %s", e)
+        raise
+
+    cat = get_parent_category_and_child_category(customer_request, categories)
+
+
+if __name__ == "__main__":
+    process_file()
